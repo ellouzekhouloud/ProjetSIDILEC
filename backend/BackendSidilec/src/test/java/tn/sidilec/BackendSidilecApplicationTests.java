4ee48a72d27570f0000f65c1f790d815500ab8ac
Khouloud Ellouze <khouloudellouze.iit@gmail.com>
2025-06-09 11:20:57 +0100

    TestUnitaire1

--- backend/BackendSidilec/src/test/java/tn/sidilec/BackendSidilecApplicationTests.java
+++ backend/BackendSidilec/src/test/java/tn/sidilec/BackendSidilecApplicationTests.java
@@ -3,7 +3,7 @@ package tn.sidilec;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendSidilecApplication.class)
 class BackendSidilecApplicationTests {
 
 	@Test
